fccdfcdcf389c88927ad969d0d407af039155546
Amit Pandey <pamit7407084@gmail.com>
2024-01-22 12:00:03 +0530

    started nextjs

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+'use client'
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Home Page</h1>
+      <button className={styles.button} onClick={() => alert("hello world")}>
+        Click
+      </button>
+    </main>
+  );
+}
